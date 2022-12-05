565b28b9d64cee7e6ebd75e704d861d96cd321ab
Karen Li <karenli@princeton.edu>
2022-12-05 18:19:58 -0500

    check user input by phrase instead of line

--- database.py
+++ database.py
@@ -13,5 +13,6 @@ def get_text(lesson, sublesson):
     collection = db.conversations
     res = collection.find_one({"lesson": lesson})["sublessons"][sublesson-1]
     text = res["text"]
-    processed_text = text.split("\n")
+    lines = text.split("\n")
+    processed_text = [line.split(" ") for line in lines]
     return {"title_kor": res["title_kor"], "title_en": res["title_en"], "text": processed_text}
