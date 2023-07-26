8d82bf519069013eb1f7cf5db8cab6a9dcdae906
Karen Li <karenli@princeton.edu>
2023-07-26 18:10:46 -0400

    create interface for admins to add and delete lessons

--- app.py
+++ app.py
@@ -55,8 +55,10 @@ def keyboard():
 @app.route('/lessons', methods=['GET'])
 def lessons():
     netid = _cas.authenticate()
+    admins = db.get_admins()
+    isadmin = netid in admins
     lessons = db.get_lessons()
-    html = render_template('lessons.html', lessons=lessons, netid=netid)
+    html = render_template('lessons.html', lessons=lessons, netid=netid, isadmin=isadmin)
     response = make_response(html)
     return response
 
@@ -66,16 +68,19 @@ def practice(lesson_num):
     lesson = int(lesson_num.split(".")[0])
     sublesson = int(lesson_num.split(".")[1])
     prev, next = None, None
-    if lesson_num == "1.1":
-        next = "/lessons/1.2"
-    elif lesson_num == "7.2":
-        prev = "/lessons/7.1"
+    existing_lessons = db.get_existing_lesson_nums()
+    existing_lessons.sort()
+    index = existing_lessons.index(lesson)
+    if index == 0 and sublesson == 1:
+        next = "/lessons/" + str(lesson) + ".2"
+    elif index == len(existing_lessons)-1 and sublesson == 2:
+        prev = "/lessons/" + str(lesson) + ".1"
     else:
         if sublesson == 2:
             prev = "/lessons/" + str(lesson) + "." + "1"
-            next = "/lessons/" + str(lesson+1) + "." + "1"
+            next = "/lessons/" + str(existing_lessons[index+1]) + "." + "1"
         else:
-            prev = "/lessons/" + str(lesson-1) + "." + "2"
+            prev = "/lessons/" + str(existing_lessons[index-1]) + "." + "2"
             next = "/lessons/" + str(lesson) + "." + "2"
     text = db.get_lesson_text(lesson, sublesson)
     convo = db.get_conversation(lesson, sublesson)
@@ -83,3 +88,62 @@ def practice(lesson_num):
     html = render_template('practice.html', lesson_num=lesson_num, text=text, prev=prev, next=next, num_characters=num_characters, netid=netid)
     response = make_response(html)
     return response
+
+@app.route('/editlessons', methods=['GET'])
+def editlessons():
+    netid = _cas.authenticate()
+    admins = db.get_admins()
+    isadmin = netid in admins
+    if not isadmin:
+        return redirect(url_for('index'))
+    lessons = db.get_lessons()
+    html = render_template('editlessons.html', lessons=lessons, netid=netid, isadmin=isadmin)
+    response = make_response(html)
+    return response
+
+
+@app.route('/makelesson', methods=['GET'])
+def makelesson():
+    netid = _cas.authenticate()
+    admins = db.get_admins()
+    isadmin = netid in admins
+    existing_lessons = db.get_existing_lesson_nums()
+    if not isadmin:
+        return redirect(url_for('index'))
+    html = render_template('makelesson.html', netid=netid, isadmin=isadmin, existing_lessons=existing_lessons)
+    response = make_response(html)
+    return response
+
+@app.route('/addlesson', methods=['GET','POST'])
+def addlesson():
+    netid = _cas.authenticate()
+    admins = db.get_admins()
+    isadmin = netid in admins
+    if not isadmin:
+        return redirect(url_for('index'))
+    dic = request.form
+
+    lesson_info = {"lesson": int(dic["lesson"]),
+                    "title_kor": dic["lesson_title_kor"],
+                    "title_en": dic["lesson_title_en"],
+                    "sublessons": [{"sublesson": 1,
+                                    "title_kor": dic["sublesson1_title_kor"],
+                                    "title_en": dic["sublesson1_title_en"],
+                                    "text": dic["sublesson1_text"]},
+                                    {"sublesson": 2,
+                                    "title_kor": dic["sublesson2_title_kor"],
+                                    "title_en": dic["sublesson2_title_en"],
+                                    "text": dic["sublesson2_text"]}]}
+    db.insert_lesson(lesson_info)
+    return redirect(url_for('lessons'))
+
+@app.route('/removelesson', methods=['GET','POST'])
+def removelesson():
+    netid = _cas.authenticate()
+    admins = db.get_admins()
+    isadmin = netid in admins
+    if not isadmin:
+        return redirect(url_for('index'))
+    lesson = int(request.args.get("lesson"))
+    db.delete_lesson(lesson)
+    return redirect(url_for('lessons'))
